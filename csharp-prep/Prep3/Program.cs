b0ab1d01bf8ac5da42697f440aa7e99e8bf56a15
Abinadi2007 <josephabinadi@gmail.com>
2024-10-05 17:45:31 -0600

    modified:   csharp-prep/Prep3/Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes"; // Variable to track if the user wants to play again
+
+        while (playAgain.ToLower() == "yes") // Loop for restarting the game
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+            // Main game loop
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You did it!");
+                }
+            }
+
+            // Stretch Challenge
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+
+        // Stretch Challenge
+        Console.WriteLine("Thank you for playing!");
     }
 }
