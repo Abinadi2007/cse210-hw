f1671d8973ac5650b8335958c43a9750dc6e0ef4
Abinadi2007 <josephabinadi@gmail.com>
2024-10-05 21:21:42 -0600

    new file:   prepare/Learning02/Job.cs 	modified:   prepare/Learning02/Program.cs 	new file:   prepare/Learning02/Resume.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "International Comunication";
+        job1._company = "World Trade Center";
+        job1._startYear = 1973;
+        job1._endYear = 2001;
+
+        Job job2 = new Job();
+        job2._jobTitle = "International Tourism Manager";
+        job2._company = "World Trade Center";
+        job2._startYear = 1978;
+        job2._endYear = 2001;
+
+        Resume myResume = new Resume();
+        myResume._name = "Claire Yamasaki";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
